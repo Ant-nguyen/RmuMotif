839a2bc3bbe54eb3321d8e69973fae5ead53a8ef
Ant-nguyen <77247395+Ant-nguyen@users.noreply.github.com>
2021-04-25 20:42:48 -0400

    edits

--- R/data.R
+++ R/data.R
@@ -1,4 +1,4 @@
-#' Dormacy Survival regulartors (DosR) upstream genes
+#' Dormancy Survival regulators (DosR) upstream genes
 #'
 #' These are 10 genes that express significant
 #' changes during hypoxic condition. For our example we can use these data to see if we can
@@ -8,3 +8,11 @@
 #'
 #' @source \url{http://bioinformaticsalgorithms.com/data/challengedatasets/DosR.txt}
 "DosR_Dataset"
+
+#'Simple sample motif data
+#'
+#'A collection of Motifs that are small and simple enough to showcase functions in muMotif
+#'
+#'@format A vector of 5 motifs 12 sequences long
+#'
+"simpleSampleMotif"
